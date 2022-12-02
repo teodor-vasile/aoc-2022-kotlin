0b9d450284ccde6214772f197d421ec20fc7b351
TeodorV <vasileteodor@icloud.com>
2022-12-02 15:07:12 +0100

    part 2 solved

--- src/main/kotlin/Day001.kt
+++ src/main/kotlin/Day001.kt
@@ -1,7 +1,18 @@
 class Day001 {
     fun part1(input: String): Int {
-        val data = input.split("\n\n")
-            .map { elf -> elf.lines().map { it.toInt() } }
+        val data = parseInput(input)
         return data.maxOf { it.sum() }
     }
+
+    fun part2(input: String): Int {
+        val data = parseInput(input)
+        return data
+            .map { it.sum() }
+            .sortedDescending()
+            .take(3)
+            .sum()
+    }
+
+    private fun parseInput(input: String) = input.split("\n\n")
+        .map { elf -> elf.lines().map { it.toInt() } }
 }
--- src/test/kotlin/Day001Test.kt
+++ src/test/kotlin/Day001Test.kt
@@ -7,7 +7,14 @@ internal class Day001Test {
     fun day01Part1() {
     val testInput = readInput("Day01Part1")
         val day001 = Day001()
-        assertEquals(24000, day001.part1(testInput))
+        assertEquals(66306, day001.part1(testInput))
+    }
+
+    @Test
+    fun day01Part2() {
+    val testInput = readInput("Day01Part1")
+        val day001 = Day001()
+        assertEquals(45000, day001.part2(testInput))
     }
 
 }
