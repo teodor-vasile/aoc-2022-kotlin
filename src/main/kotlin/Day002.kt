ba437d98b80965659a53fceefc158d9996281980
TeodorV <vasileteodor@icloud.com>
2022-12-02 18:09:23 +0100

    Day2Part2 solved

--- src/main/kotlin/Day002.kt
+++ src/main/kotlin/Day002.kt
@@ -1,16 +1,14 @@
 class Day002 {
     fun part1(input: String): Int {
-        val checkMap: MutableMap<String, Int> = mutableMapOf()
-        checkMap["B X"] = 1
-        checkMap["B X"] = 1
-        checkMap["C Y"] = 2
-        checkMap["A Z"] = 3
-        checkMap["A X"] = 4
-        checkMap["B Y"] = 5
-        checkMap["C Z"] = 6
-        checkMap["C X"] = 7
-        checkMap["A Y"] = 8
-        checkMap["B Z"] = 9
+        val checkMap: Map<String, Int> = mapOf(Pair("B X", 1), Pair("C Y", 2), Pair("A Z", 3),
+            Pair("A X", 4), Pair("B Y", 5), Pair("C Z", 6), Pair("C X", 7), Pair("A Y", 8), Pair("B Z", 9))
+
+        return parseInput(input).sumOf { checkMap.getValue(it) }
+    }
+
+    fun part2(input: String): Int {
+        val checkMap: Map<String, Int> = mapOf(Pair("B X", 1), Pair("C Y", 6), Pair("A Z", 8),
+            Pair("A X", 3), Pair("B Y", 5), Pair("C Z", 7), Pair("C X", 2), Pair("A Y", 4), Pair("B Z", 9))
         return parseInput(input).sumOf { checkMap.getValue(it) }
     }
 
--- src/test/kotlin/Day002Test.kt
+++ src/test/kotlin/Day002Test.kt
@@ -9,6 +9,13 @@ internal class Day002Test {
         val day002 = Day002()
         assertEquals(10624, day002.part1(testInput))
     }
-    
+
+    @Test
+    fun day02part2() {
+    val testInput = readInput("Day02part1")
+        val day002 = Day002()
+        assertEquals(12, day002.part2(testInput))
+    }
+
 
 }
