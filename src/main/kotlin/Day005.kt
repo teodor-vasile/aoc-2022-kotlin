cdeceb1cdf69b5b4c950c6088b853a643cce1d18
TeodorV <vasileteodor@icloud.com>
2022-12-06 23:35:27 +0100

    Day05Part2 solved

--- src/main/kotlin/Day005.kt
+++ src/main/kotlin/Day005.kt
@@ -1,24 +1,46 @@
 class Day005 {
 
+    data class Instruction(val count: Int, val source: Int, val target: Int)
+
     fun part1(lines: List<String>): String {
 
-        // get the number of containers
         val numberOfContainers = lines.first { it.trim().startsWith("1") }.trim().last().digitToInt()
-        // create containers
-        val containers = List(numberOfContainers) { ArrayDeque<Char>() }
 
-        lines
-            .takeWhile { !it.trim().startsWith("1") }
-            .map { line ->
-                line.slice(1..line.length step 4)
-                    .mapIndexed { containerNumber, value ->
-                        if (value.isLetter()) containers[containerNumber].addLast(value)
-                    }
+        val containers = List(numberOfContainers) { ArrayDeque<Char>() }
+        createContainers(lines, containers as MutableList<ArrayDeque<Char>>)
+
+
+
+        val instructions: List<Instruction> = getInstructions(lines)
+
+        instructions.forEach { instruction ->
+            repeat (instruction.count) {
+                if (containers[instruction.source - 1].isNotEmpty())
+                    containers[instruction.target - 1].addFirst(containers[instruction.source - 1].removeFirst())
+            }
+        }
+
+        return containers.map { it.first() }.joinToString("")
+    }
+
+    fun part2(lines: List<String>): String {
+
+        val numberOfContainers = lines.first { it.trim().startsWith("1") }.trim().last().digitToInt()
+        val containers = List(numberOfContainers) { ArrayDeque<Char>() }.toMutableList()
+
+        createContainers(lines, containers)
+
+        val instructions: List<Instruction> = getInstructions(lines)
+
+        instructions.forEach { instr ->
+            containers[instr.target - 1].addAll(0, containers[instr.source - 1].take(instr.count))
+                    repeat(instr.count) {containers[instr.source - 1].removeFirst()}
             }
 
-        // get instructions out
-        data class Instruction(val count: Int, val source: Int, val target: Int)
+        return containers.map { it.first() }.joinToString("")
+    }
 
+    private fun getInstructions(lines: List<String>): List<Instruction> {
         val regex = """move (\d+) from (\d+) to (\d+)""".toRegex()
         val instructions: List<Instruction> = lines
             .dropWhile { !it.trim().startsWith("move") }
@@ -27,36 +49,21 @@ class Day005 {
                 val (count, source, target) = matchResult!!.destructured
                 Instruction(count.toInt(), source.toInt(), target.toInt())
             }
+        return instructions
+    }
 
-        println("Before ----------")
-        containers.forEach { println(it) }
-        println(containers.size)
-        instructions.forEach { println(it) }
-        instructions.forEach {
-                println(it)
-            for (i in 1..it.count) {
-                if (containers[it.source - 1].isNotEmpty())
-                containers[it.target - 1].addFirst(containers[it.source - 1].removeFirst())
-            println(containers.forEach { println(it) })
-                println(containers.size)
+    private fun createContainers(
+        lines: List<String>,
+        containers: MutableList<ArrayDeque<Char>>
+    ) {
+        lines
+            .takeWhile { !it.trim().startsWith("1") }
+            .map { line ->
+                line.slice(1..line.length step 4)
+                    .mapIndexed { containerNumber, value ->
+                        if (value.isLetter()) containers[containerNumber].addLast(value)
+                    }
             }
-        }
-        println("After ----------")
-
-        containers.forEach { println(it) }
-
-        containers.forEach {
-            println(it.first())
-
-        }
-
-        val str =
-            containers.map{
-                it.first()
-            }
-                .joinToString ("")
-
-        return str
     }
 
 }
--- src/test/kotlin/Day005Test.kt
+++ src/test/kotlin/Day005Test.kt
@@ -11,5 +11,11 @@ internal class Day005Test {
         assertEquals("CNSZFDVLJ", day005.part1(testInput))
     }
 
+    @Test
+    fun day05part2() {
+        val testInput = readInputLines("Day05part1")
+        assertEquals("QNDWLMGNS", day005.part2(testInput))
+    }
+
 
 }
